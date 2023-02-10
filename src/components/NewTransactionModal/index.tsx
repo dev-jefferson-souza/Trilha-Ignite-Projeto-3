62f820239c18040e279dc50c2332e3584a98e7d8
Jefferson Souza <jefferson.souza@neki-it.com.br>
2023-02-10 14:56:48 -0300

    feat(Axios): requisição ordenada

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as Dialog from '@radix-ui/react-dialog'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { useContext } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import * as z from 'zod'
+import { TransactionsContext } from '../../context/TransactionsContext'
 import {
   CloseButton,
   Content,
@@ -21,10 +23,12 @@ const newTransactionFormShcema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormShcema>
 
 export function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext)
   const {
     control,
     register,
     handleSubmit,
+    reset,
     formState: { isSubmitting },
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormShcema),
@@ -34,9 +38,15 @@ export function NewTransactionModal() {
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    const { description, price, category, type } = data
 
-    console.log(data)
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    })
+    reset()
   }
 
   return (
